63a5b9f57c3957967e8de5b906943bae7aac7bb3
Mansoury <=>
2020-02-24 14:43:17 +0330

    modifing snackBar component and install axios

--- src/Pages/AdminPanel/Components/ManageRoles.js
+++ src/Pages/AdminPanel/Components/ManageRoles.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import Button from '@material-ui/core/Button'
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -6,17 +6,18 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import TextField from '@material-ui/core/TextField';
-import {fade,MuiThemeProvider,withStyles,makeStyles,createMuiTheme} from '@material-ui/core/styles';
-import { green } from '@material-ui/core/colors';
+import {withStyles} from '@material-ui/core/styles';
 import ChackPageContext from './../../Context/PageSizeChecking';
 import SnackBarComponent from './SnackBarComponent';
+import axios from 'axios';
+import Axios from 'axios';
 
-const theme = createMuiTheme({
-    palette:{
-        primary:green
-    },
-    direction:'ltr',
-})
+// const theme = createMuiTheme({
+//     palette:{
+//         primary:green
+//     },
+//     direction:'ltr',
+// })
 
 const CssTextField = withStyles({
     root: {
@@ -59,11 +60,21 @@ const ManageRoles = () =>{
     const context =useContext(ChackPageContext);
     let {isSMMode} = context;
 
-    const handleClick = ()=>{
-      console.log("Clicked");
-    }
+    const childRef = useRef();
 
-    
+
+    const handleClick = childRef =>()=>{
+      Axios.post("http://localhost:53094/api/userapi/Login",{
+        userName: 'usernameL',
+        password: 'passwordL'
+      }).then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });;
+      childRef.current.showSnackBar("hello");
+    }
     return (
         <div>
             <ExpansionPanel>
@@ -77,20 +88,18 @@ const ManageRoles = () =>{
                   <div style={{display: 'flex',flexDirection:'column'}}>
                     <div>
                         <CssTextField
-                                label ="نام نقش"
+                                label ="Role Name"
                                 variant="outlined"
                                 id="lblCreateRole"
                             ></CssTextField>
                     </div>
                     <div style={Object.assign(isSMMode?{justifyContent:'center'}:{justifyContent:'right'},{marginTop:'10px',display: 'flex'})}>
-                        <Button variant="contained" color="secondary" onClick={handleClick}>Secondary</Button>
+                        <Button onClick={handleClick(childRef)} variant="contained" color="secondary">Create Role</Button>
                     </div>
                   </div>
-
                 </ExpansionPanelDetails>
-
             </ExpansionPanel>
-            <SnackBarComponent></SnackBarComponent>
+            <SnackBarComponent ref={childRef}></SnackBarComponent>
         </div>
           
     )
--- src/Pages/AdminPanel/Components/SnackBarComponent.js
+++ src/Pages/AdminPanel/Components/SnackBarComponent.js
@@ -1,49 +1,54 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { forwardRef ,useImperativeHandle} from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 import Fade from '@material-ui/core/Fade';
-import Slide from '@material-ui/core/Slide';
+// import Slide from '@material-ui/core/Slide';
 import Grow from '@material-ui/core/Grow';
 
-function SlideTransition(props) {
-  return <Slide {...props} direction="up" />;
-}
+// function SlideTransition(props) {
+//   return <Slide {...props} direction="up" />;
+// }
 
 function GrowTransition(props) {
   return <Grow {...props} />;
 }
 
-export default function TransitionsSnackbar() {
-  const [state, setState] = React.useState({
-    open: false,
-    Transition: Fade,
-  });
+const TransitionsSnackbar =forwardRef((props,ref) =>{
 
-  const handleClick = Transition => () => {
-    setState({
-      open: true,
-      Transition:GrowTransition,
+    const [state, setState] = React.useState({
+        open: false,
+        Transition: Fade,
+        message:""
     });
-  };
 
-  const handleClose = () => {
-    setState({
-      ...state,
-      open: false,
-    });
-  };
+    useImperativeHandle(ref,()=>({
+        showSnackBar(message){
+            setState({
+                open: true,
+                Transition:GrowTransition,
+                message:message
+            });
+        }
+    }));
 
-  return (
-    <div>
-      {/* <Button onClick={handleClick(GrowTransition)}>Grow Transition</Button>
-      <Button onClick={handleClick(Fade)}>Fade Transition</Button>
-      <Button onClick={handleClick(SlideTransition)}>Slide Transition</Button> */}
-      <Snackbar
-        open={state.open}
-        onClose={handleClose}
-        TransitionComponent={state.Transition}
-        message="I love snacks"
-      />
-    </div>
-    );
-}
+    
+      const handleClose = () => {
+        setState({
+          ...state,
+          open: false,
+        });
+      };
+    
+      return (
+        <div>
+          <Snackbar
+            open={state.open}
+            onClose={handleClose}
+            TransitionComponent={state.Transition}
+            message={state.message}
+            
+          />
+        </div>
+        );
+}); 
+
+export default TransitionsSnackbar;
